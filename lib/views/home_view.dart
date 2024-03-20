74717e4f61952db4315af42073042efd1b4843a9
Joel Torres <thereaderboy.7@outlook.com>
2024-03-20 15:54:14 -0500

    SMP-002 - Redirects to HomeView if user data exists.

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,13 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class HomeView extends StatelessWidget {
+class HomeView extends StatefulWidget {
   const HomeView({super.key});
 
   @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  late String? userName;
+
+  @override
+  void initState() {
+    userName = '';
+    super.initState();
+    _loadUserData();
+  }
+
+  void _loadUserData() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      userName = prefs.getString('userName') ?? '';
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+
+    return Scaffold(
       body: SafeArea(
-          child: Text("This is the HomeView.")
+          child: Text("Welcome, ${userName}")
       ),
     );
   }
